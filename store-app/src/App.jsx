d7f2dc3d9bbad6e29ff934831e752f84a690e52e
alper <alperyasar@gtu.edu.tr>
2025-05-17 20:14:32 +0300

    update

--- store-app/src/App.jsx
+++ store-app/src/App.jsx
@@ -1,8 +1,55 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
+import CartsPage from "./pages/Carts";
+import LoginPage from "./pages/Login";
+import RegisterPage from "./pages/Register";
+import ProductsDetailsPage from "./pages/ProductsDetailsPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "home",
+        element: <HomePage />,
+      },
+      {
+        path: "products",
+        children: [
+          {
+            index: true,
+            element: <ProductsPage />,
+          },
+          {
+            path: ":id",
+            element: <ProductsDetailsPage />,
+          },
+        ],
+      },
+      {
+        path: "carts",
+        element: <CartsPage />,
+      },
+      {
+        path: "login",
+        element: <LoginPage />,
+      },
+      {
+        path: "register",
+        element: <RegisterPage />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return (
-    <div className="App">
-      <h1>Store App</h1>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 export default App;
